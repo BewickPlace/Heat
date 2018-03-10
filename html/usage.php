742baf91fe7f61ac67079489899b8381786a9470
BewickPlace <John.J.Chandler@tesco.net>
2018-03-10 18:36:12 +0000

    Fix: tidy up image handling on slave

--- html/usage.php
+++ html/usage.php
@@ -85,9 +85,12 @@ $submenu_mode="";
 	$filename = $node.'.png';
 ?>
     <p>
-    <?php if (file_exists($filename)) { unlink($filename);} ?>
-    <?php generate_graph($hostname, NULL, $node, $selected_date, 1); ?>
-    <img src=<?php echo $filename ?> height=50% width=100%>
+<?php
+    if (file_exists($filename)) { unlink($filename);}
+    if (generate_graph($hostname, NULL, $node, $selected_date, 1) == 0) {
+     ?> <img src=<?php echo $filename ?> height=50% width=100%><?php
+    }
+?>
     </p>
 
 <?php
