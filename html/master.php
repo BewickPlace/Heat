189b7d8ad4d3b3fae19c098e510536043ccb0c78
BewickPlace <John.J.Chandler@bewickplace.co.uk>
2022-04-02 18:13:40 +0100

    Add: monthly average hours run

--- html/master.php
+++ html/master.php
@@ -6,6 +6,7 @@
 <?php
 require 'html_functions.php';
 require 'functions.php';
+require 'graph.php';
 $hostname = getmyhostname();
 print("<title>".$hostname.": Temperature Control System</title>");
 ?>
@@ -36,6 +37,8 @@ require 'manage_menu.php';
 #
     $time = time();
     $date = date('Y-m-d', $time);
+    $year = strtok($date, "-");
+    $month = strtok("-");
 
     $system = get_system_name();
     $zones = get_zone_names();
@@ -48,7 +51,10 @@ require 'manage_menu.php';
     <div>
 <?php
     $hours_run = get_hours_run($hostname, $date, $status);
-    echo "Daily total of Hours Run: ", $hours_run,"<br><br>";
+    echo "Daily total of Hours Run: ", $hours_run,"<br>";
+
+    $hours_run = round(get_monthly_avg($hostname, $year, $month),1);
+    echo "Average daily usage this month: ", $hours_run, "Kwh<br><br>";
 ?>
     </div>
     <div>
